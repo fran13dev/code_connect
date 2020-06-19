1860275558b9715dd0ace5ff9b326670ae99bee3
Francois de Villiers <fran13dev@gmail.com>
2020-06-19 16:26:32 +0200

    Updated config

---
+++ config/keys.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mongodb: {
+    uri:
+      'mongodb+srv://fdevilliers134:Francois@frdevi134-j0003.mongodb.net/devconnect?retryWrites=true&w=majority'
+  },
+  jwt: {
+    secret: 'devConnectSecretToken'
+  },
+  github: {
+    clientId: '2896b6a6152a63d08911',
+    clientSecret: '00d7a73144b636581784d2bee18653e3eb2b2917'
+  }
+}
